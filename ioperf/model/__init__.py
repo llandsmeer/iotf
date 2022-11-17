24ea698f5151d2105360e36ce664561bdcb6fe8f
Lennart Landsmeer <lennart@landsmeer.email>
2022-11-17 12:10:35 +0100

    condense docs a bit

--- ioperf/model/__init__.py
+++ ioperf/model/__init__.py
@@ -3,3 +3,11 @@ from .make_initial_neuron_state import make_initial_neuron_state
 from .sample_connections_3d import sample_connections_3d
 from .timestep import timestep
 from .make_onnx_model import make_onnx_model
+
+__all__ = [
+        'make_tf_function',
+        'make_initial_neuron_state',
+        'sample_connections_3d',
+        'timestep',
+        'make_onnx_model'
+        ]
--- ioperf/runners/__init__.py
+++ ioperf/runners/__init__.py
@@ -2,7 +2,18 @@ from . import onnx_experiment
 from . import onnx_runner
 from . import graphcore
 
+__all__ = [
+        'onnx_runner',
+        'runners',
+        'graphcore',
+        'onnx_experiment'
+        ]
+
 runners = {
     'onnx': onnx_runner.main,
     'graphcore': graphcore.main
 }
+'''
+Dictionary of runners. Keys correspond to
+--runner argument of __main__.py.
+'''
