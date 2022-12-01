b9ca99e834d762efa6ffaabe418a7699d73f1b59
Lennart Landsmeer <lennart@landsmeer.email>
2022-12-01 10:56:44 +0100

    energy bench

--- ioperf/__main__.py
+++ ioperf/__main__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import socket
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import argparse
@@ -7,6 +8,7 @@ import socket
 import subprocess
 import datetime
 from .bench.benchmark import Benchmark
+from .bench.energy_bench import EnergyBenchmark
 import os.path
 from os import path
 
@@ -19,7 +21,12 @@ def get_git_revision_short_hash() -> str:
 
 parser = argparse.ArgumentParser()
 
-def main():
+def bench_with_ipu_shim():
+    from .runners.tests.test_graphcore_runner_with_shim import IPU_Module_Shim
+    with IPU_Module_Shim.register():
+        main()
+
+def get_benchmark_filename():
     hostname = socket.gethostname()
     gitHash = get_git_revision_short_hash()
     date = datetime.datetime.now().strftime('%m-%d-%Y')    
@@ -31,9 +38,48 @@ def main():
     while path.exists(log_file):
         count = count +1
         log_file = log_file_o + "_" + str(count) + ".dat"
+    return log_file
+
+def bench():
+    log_file = get_benchmark_filename()
     print('logging to', log_file)
     benchmark = Benchmark(log_file=log_file)
+    benchmark.plot = False
+    benchmark.run_unconnected = True
+    benchmark.run_connected = True
     benchmark.run()
+
+def run_energy_bench():
+    log_file = get_benchmark_filename() + '.energy'
+    print('logging to', log_file)
+    benchmark = EnergyBenchmark(log_file=log_file)
+    benchmark.run()
+
+def usage():
+    print('usage:', sys.argv[0], '<cmd>')
+    print('cmd:')
+    for k, v in CMDS.items():
+        print(' '*10, k)
+
+CMDS = {
+        '-h': usage,
+        '--help': usage,
+        'bench': bench,
+        'bench-shim': bench_with_ipu_shim,
+        'energy': run_energy_bench
+}
+
+def main():
+    if len(sys.argv) > 2:
+        print('supply cmd or no args')
+        exit(1)
+    cmd = sys.argv[1] if len(sys.argv) == 2 else 'bench'
+    f = CMDS.get(cmd)
+    if f is None:
+        usage()
+        exit(1)
+    else:
+        f()
 
 if __name__ == '__main__':
     main()
---
+++ ioperf/bench/energy_bench.py
@@ -0,0 +1,72 @@
+import multiprocessing as mp
+import time
+import subprocess
+import os
+
+from .. import runners
+from .model_configuration import ModelConfiguration
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def measurement_loop(runner, exe, log_file):
+    a = time.perf_counter()
+    i = 0
+    while True:
+        print(i)
+        b = time.perf_counter()
+        res = subprocess.getoutput(exe)
+        with open(log_file, 'a') as f:
+            for line in res.splitlines():
+                print(runner, i, f'{b - a:10.5f}', line, file=f)
+        i += 1
+        time.sleep(1)
+
+
+class EnergyBenchmark:
+    def __init__(self, log_file):
+        self.config = ModelConfiguration.create_new(nneurons=9**3, seed=1234)
+        self.runners = [
+            (runners.GraphcoreRunner,    'gc-monitor'),
+            (runners.tf_base_runner,     'nvidia-smi'),
+            #(runners.TfBaseRunner,       'cat /sys/class/power_supply/BAT0/uevent'),
+            (runners.OnnxCUDARunner,     'nvidia-smi'),
+            (runners.OnnxTensorRTRunner, 'nvidia-smi'),
+            (runners.OnnxTensorRTRunner, 'nvidia-smi'),
+        ]
+        self.log_file = log_file
+        self.hostname = subprocess.getoutput('hostname').strip()
+
+    def log(self, *args):
+        raw = ' '.join(map(str, args))
+        print(bcolors.OKGREEN + raw[:100] + bcolors.ENDC)
+        with open(self.log_file, 'a') as f:
+            print(raw, file=f)
+
+    def run(self):
+        config = self.config
+        for Runner, exe in self.runners:
+            runner = Runner()
+            if not runner.is_supported():
+                self.log('unsupported', type(runner).__name__)
+                continue
+            if os.system(f'which {exe}') != 0:
+                self.log('unsupported', type(runner).__name__, 'not-found:', exe)
+            self.log('energy', config)
+            runner.setup_using_model_config(config, gap_junctions=True)
+            # burn jit:
+            runner.run_with_gap_junctions(1000, config.state, gj_src=config.gj_src, gj_tgt=config.gj_tgt)
+            t = mp.Process(target=measurement_loop, args=(type(runner).__name__, exe, self.log_file))
+            t.start()
+            runner.run_with_gap_junctions(10000, config.state, gj_src=config.gj_src, gj_tgt=config.gj_tgt)
+            t.terminate()
+            t.join()
+
